b2c20c3a3227debd853ba39cf34857e8837a1290
sasa.teofanovic@aimit.at <sasa.teofanovic@aimit.at@2c731a20-4d88-4a92-a529-68eaa7a3bac2>
2013-12-17 14:32:11 +0000

    Max. expressions for in-condition (e.g Oracle-Limit 1000 IN values)

--- trunk/org.mariella.persistence/src/org/mariella/persistence/query/InCondition.java
+++ trunk/org.mariella.persistence/src/org/mariella/persistence/query/InCondition.java
@@ -3,25 +3,48 @@ package org.mariella.persistence.query;
 import java.util.List;
 
 public class InCondition implements Expression {
+	
 	private Expression leftExpression;
 	private List<Expression> in;
+	private int maxInExpressions = 1000;
 	
-public InCondition(Expression leftExpression, List<Expression> in) {
-	super();
-	this.leftExpression = leftExpression;
-	this.in = in;
-}
-	
-public void printSql(StringBuilder b) {
-	leftExpression.printSql(b);
-	b.append(" IN (");
-	boolean first = true;
-	for(Expression expression : in) {
-		if(first) first = false;
-		else b.append(", ");
-		expression.printSql(b);
+	public InCondition(Expression leftExpression, List<Expression> in) {
+		super();
+		this.leftExpression = leftExpression;
+		this.in = in;
 	}
-	b.append(")");
-}
+	
+	public int getMaxInExpressions() {
+		return maxInExpressions;
+	}
+	
+	public void setMaxInExpressions(int maxInExpressions) {
+		if (maxInExpressions <= 0) {
+			throw new IllegalArgumentException();
+		}
+		this.maxInExpressions = maxInExpressions;
+	}
+	
+	public void printSql(StringBuilder b) {
+		if (in.size() > maxInExpressions) {
+			b.append("(");
+		}
+		leftExpression.printSql(b);
+		b.append(" IN (");
+		int i = 0;
+		for (Expression expression : in) {
+			if (i == maxInExpressions) {
+				b.append(") OR ");
+				leftExpression.printSql(b);
+				b.append(" IN (");
+				i = 0;
+			} else if (i > 0) {
+				b.append(", ");
+			}
+			expression.printSql(b);
+			i++;
+		}
+		b.append(in.size() > maxInExpressions ? "))" : ")");
+	}
 
 }
