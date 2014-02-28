5b1c479fe54f34f438c5d32729352ab5ef8c4472
martin.trummer@aimit.at <martin.trummer@aimit.at@2c731a20-4d88-4a92-a529-68eaa7a3bac2>
2014-02-28 18:22:47 +0000

    Batched Updates/Inserts

--- org.mariella.persistence/src/org/mariella/persistence/mapping/IBatchStrategy.java
+++ org.mariella.persistence/src/org/mariella/persistence/mapping/IBatchStrategy.java
@@ -6,7 +6,7 @@ public interface IBatchStrategy {
 	 * @return The persistent classes to use batched update/insert.
 	 * The returned classes are persisted at the end of a save-cycle in the order they are returned. 
 	 */
-	Class<?>[] getOrderedBatchClasses();
+	Object[] getOrderedBatchClasses();
 	
 	/**
 	 * @return The maximum number of overall rows batched before executing the batched statements.
