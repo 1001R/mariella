e5e3adf80844aa635278ee9805a0cb4f6f1988cf
martin.trummer@aimit.at <martin.trummer@aimit.at@2c731a20-4d88-4a92-a529-68eaa7a3bac2>
2015-02-01 17:28:47 +0000

    Support of PostgreSQL

--- trunk/org.mariella.persistence.oracle/src/org/mariella/persistence/oracle/OraclePersistenceBuilder.java
+++ trunk/org.mariella.persistence.oracle/src/org/mariella/persistence/oracle/OraclePersistenceBuilder.java
@@ -1,23 +1,19 @@
 package org.mariella.persistence.oracle;
 
-import java.sql.Types;
-
 import org.mariella.persistence.annotations.mapping_builder.DatabaseInfoProvider;
 import org.mariella.persistence.annotations.mapping_builder.PersistenceBuilder;
 import org.mariella.persistence.annotations.mapping_builder.PersistenceInfo;
-import org.mariella.persistence.annotations.mapping_builder.ConverterRegistryImpl.ConverterFactoryImpl;
 import org.mariella.persistence.mapping.OxyUnitInfo;
 
 public class OraclePersistenceBuilder extends PersistenceBuilder {
 
-public OraclePersistenceBuilder(OxyUnitInfo unitInfo, DatabaseInfoProvider databaseInfoProvider) {
-	super(unitInfo, databaseInfoProvider);
-	converterRegistry.registerConverterFactory(Types.CLOB, String.class, new ConverterFactoryImpl(ClobConverter.Singleton));
-}
-
-@Override
-protected PersistenceInfo createPersistenceInfo() {
-	return new PersistenceInfo(new OracleSchema());
-}
+	public OraclePersistenceBuilder(OxyUnitInfo unitInfo, DatabaseInfoProvider databaseInfoProvider) {
+		super(unitInfo, databaseInfoProvider);
+	}
+	
+	@Override
+	protected PersistenceInfo createPersistenceInfo() {
+		return new PersistenceInfo(new OracleSchema());
+	}
 
 }
